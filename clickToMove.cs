8368ac45b0265459b056e88e28773fe5e2b24631
yeen0 <102559375+yeen0@users.noreply.github.com>
2024-02-19 21:19:31 -0300

    Add files via upload

---
+++ clickToMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickToMove : MonoBehaviour
+{
+    [SerializeField] public float speed = 10f;
+    Vector2 lastClickedPos;
+    bool moving;
+
+    private void Update() 
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+
+        if(moving && (Vector2)transform.position != lastClickedPos)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+}
